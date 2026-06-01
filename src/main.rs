4b6b356892fce54b575574561352d8d1e8ee169f
Ahmed Hamza Aydin <ahamzaaydin.py@gmail.com>
2026-06-01 18:53:18 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,15 @@ use std::io;
 use std::io::Write;
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    let mut command = String::new();
-    io::stdin()
-        .read_line(&mut command)
-        .expect("Failed to read command");
+        let mut command = String::new();
+        io::stdin()
+            .read_line(&mut command)
+            .expect("Failed to read command");
 
-    print!("{}: command not found", command.trim());
+        println!("{}: command not found", command.trim());
+    }
 }
