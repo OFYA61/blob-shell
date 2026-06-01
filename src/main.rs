5f0c93f21c5d6fc2e8d641a39e49ea80309c7f09
Ahmed Hamza Aydin <ahamzaaydin.py@gmail.com>
2026-06-01 18:51:24 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
-#[allow(unused_imports)]
-use std::io::{self, Write};
+use std::io;
+use std::io::Write;
 
 fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut command = String::new();
+    io::stdin()
+        .read_line(&mut command)
+        .expect("Failed to read command");
+
+    print!("{}: command not found", command.trim());
 }
